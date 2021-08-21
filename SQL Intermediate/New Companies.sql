583e995fedb89606c8c7a188a4beb412c71859ce
Girish Malekar <gmalekar@hawk.iit.edu>
2021-08-21 17:23:57 -0500

    Create New Companies.sql

---
+++ SQL Intermediate/New Companies.sql
@@ -0,0 +1,12 @@
+select
+c.company_code
+,c.founder
+,count(distinct e.lead_manager_code)
+,count(distinct e.senior_manager_code)
+,count(distinct e.manager_code)
+,count(distinct e.employee_code)
+from company c
+join employee e
+on c.company_code=e.company_code
+group by 1,2
+order by 1
