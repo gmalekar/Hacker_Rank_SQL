cc57285eec5f7d6853b7bb0c126a71a459c2d583
Girish Malekar <gmalekar@hawk.iit.edu>
2021-08-21 10:41:47 -0500

    Create Weather Observation Station 10.sql

---
+++ SQL Basic/Weather Observation Station 10.sql
@@ -0,0 +1,9 @@
+/*
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+*/
+
+select city
+from station
+where right(city,1) not in ('a','e','i','o','u')
+group by 1
